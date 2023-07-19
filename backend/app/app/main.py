50ea87e64f93b85cfd9680b4facf426573fd2810
Ana Luísa Ferreira <aluisaf@ua.pt>
2023-07-19 11:32:18 +0100

    mapbox access token changed

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -47,15 +47,6 @@ testapi = FastAPI(title="Test APIs", openapi_prefix="/test")
 testapi.include_router(tests_router, prefix=settings.API_V1_STR)
 app.mount("/test", testapi)
 
-# #Middleware - add a custom header X-Process-Time containing the time in seconds that it took to process the request and generate a response
-# @app.middleware("http")
-# async def add_process_time_header(request: Request, call_next):
-#     start_time = time.time()
-#     response = await call_next(request)
-#     process_time = time.time() - start_time
-#     response.headers["X-Process-Time"] = str(process_time)
-#     return response
-
 # ================================= Static Page routes =================================
 
 # static files folder
