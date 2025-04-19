f31bfd8708942086b092eb690e9eec00060f5b68
iavcoelho <iavcoelho@ua.pt>
2025-04-19 21:02:52 +0100

    implemented endpoint to update UE location and send notification to the subscriptions

--- backend/app/app/tools/check_subscription.py
+++ backend/app/app/tools/check_subscription.py
@@ -2,7 +2,11 @@ import logging
 import time
 from app.crud import crud_mongo
 
+
 def check_expiration_time(expire_time):
+    if not expire_time:
+        return True
+
     year = int(expire_time[0:4])
     month = int(expire_time[5:7])
     day = int(expire_time[8:10])
@@ -12,31 +16,31 @@ def check_expiration_time(expire_time):
 
     time_now = time.localtime()
     # print(time.asctime(time_now))
-    
-    if year>time_now[0]: 
+
+    if year > time_now[0]:
         # print(year, time_now[0])
         return True
     elif year == time_now[0]:
         if month > time_now[1]:
             # print(month, time_now[1])
             return True
-        elif(month == time_now[1]):
-            if(day>time_now[2]):
+        elif month == time_now[1]:
+            if day > time_now[2]:
                 # print(day, time_now[2])
                 return True
-            elif(day==time_now[2]):
+            elif day == time_now[2]:
                 # print("Day == day now", day, time_now[2])
-                if(hour>time_now[3]):
+                if hour > time_now[3]:
                     # print(hour, time_now[3])
                     return True
-                elif(hour==time_now[3]):
+                elif hour == time_now[3]:
                     # print("Time == time now", hour, time_now[3])
-                    if(minute>time_now[4]):
+                    if minute > time_now[4]:
                         print(minute, time_now[4])
                         return True
-                    elif(minute==time_now[4]):
+                    elif minute == time_now[4]:
                         # print("Minute == minute now", minute, time_now[4])
-                        if(sec>=time_now[5]):
+                        if sec >= time_now[5]:
                             # print(sec, time_now[5])
                             return True
                         else:
@@ -52,9 +56,13 @@ def check_expiration_time(expire_time):
     else:
         return False
 
+
 def check_numberOfReports(maximum_number_of_reports: int) -> bool:
+    if not maximum_number_of_reports:
+        return True
+
     if maximum_number_of_reports >= 1:
         return True
-    else:
-        logging.warning("Subscription has expired (maximum number of reports")
-        return False
+
+    logging.warning("Subscription has expired (maximum number of reports")
+    return False
