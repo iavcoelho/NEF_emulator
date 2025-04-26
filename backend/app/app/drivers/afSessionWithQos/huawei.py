31ee00b51a7a850456634288fd9fe7810f2f5bf4
João Capucho <capucho@jcapucho.com>
2025-04-26 01:05:18 +0100

    Huawei QoS interface fixes

--- backend/app/app/drivers/afSessionWithQos/huawei.py
+++ backend/app/app/drivers/afSessionWithQos/huawei.py
@@ -23,6 +23,8 @@ class HuaweiAfSessionWithQos(AfSessionWithQosInterface):
         self.httpx_client = httpx.AsyncClient(
             base_url=slice_manager_api_url,
             auth=httpx.BasicAuth(username=api_user, password=api_password),
+            # The API is slow to respond, so disable timeouts
+            timeout=None
         )
         self.default_ambrup = default_ambrup
         self.default_ambrdl = default_ambrdl
@@ -34,16 +36,23 @@ class HuaweiAfSessionWithQos(AfSessionWithQosInterface):
             "IMSI": ue.supi,
             "numIMSIs": 1,
             "slice": slice,
-            "IPV4": "",
-            "IPV6": "",
             "AMDATA": True,
-            "DEFAULT": "TRUE",
-            "UEcanSendSNSSAI": "FALSE",
             "AMBRUP": ambrup,
             "AMBRDW": ambrdl,
         }
 
-        res = await self.httpx_client.patch("/UE/patch", json=payload)
+        res = await self.httpx_client.get(f"/UE/{ue.supi}/get_IMSI")
+
+        if res.is_success:
+            logging.debug("Patching UE state")
+            res = await self.httpx_client.patch("/UE/patch", json=payload)
+        elif res.status_code == 404:
+            logging.debug("Creating new UE state")
+            res = await self.httpx_client.post("/UE/post", json=payload)
+        else:
+            raise Exception(
+                f"Error while getting UE status in Huawei Core for {ue.supi} (status code: {res.status_code}): {res.text}"
+            )
 
         if res.is_success:
             logging.debug(f"Provisioned QoS in Huawei Core for {ue.supi}")
@@ -56,9 +65,20 @@ class HuaweiAfSessionWithQos(AfSessionWithQosInterface):
     async def change_qos(
         self, subscription: AsSessionWithQoSSubscription, ues: List[UE], qos: QoSProfile
     ) -> None:
+        # The Slice Manager API expects the value to be in Kbps but the
+        # interface works with bps so we need to divide by 1000 rounding up
+        ambrup = qos.uplinkBitRate
+        if ambrup is not None:
+            ambrup = (ambrup + 999) // 1000
+
+        ambrdl = qos.downlinkBitRate
+        if ambrdl is not None:
+            ambrdl = (ambrdl + 999) // 1000
+
+
         for ue in ues:
             await self._change_ue_qos(
-                ue, subscription.dnn or ue.dnn, qos.uplinkBitRate, qos.downlinkBitRate
+                ue, subscription.dnn or ue.dnn, ambrup, ambrdl
             )
 
     async def revert_qos(
